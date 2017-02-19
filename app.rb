9478f86ce74a36e426b7e3d4ea4768cc98983a93
dison  ruan <disonruan@disons-Air.fios-router.home>
2017-02-19 16:28:20 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!! I DIDN'T BUILD THIS\n
+    INITIATED WITH SHOTGUN FYI"
   end
 
 end
